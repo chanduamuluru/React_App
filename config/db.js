b8600aad6ab6a3c1b7b6e2bde2ded5081f7bf7fe
chanduamuluru <chandu.a555@gmail.com>
2019-12-23 01:22:54 +0530

    React APP with Mongodb
    server code with API using express,mongoDB

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+      console.log(config);
+      console.log(db);
+    console.error(`DB Connection Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
