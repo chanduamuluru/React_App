3b61b5eb7e14e55393d77e3083e160202e747988
chanduamuluru <chanduamuluru@.github.com>
2019-12-26 18:27:57 +0530

    React register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
 import "./App.css";
 
 const App = () => (
-  <Fragment>
-    <h1>APP</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Register" component={Register} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
